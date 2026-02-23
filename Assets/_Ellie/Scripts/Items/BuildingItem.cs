c0a285bad40bb839431a0666e5056893dd00fcd2
Ellie <Ellie>
2026-02-23 11:30:17 +0100

    Building base

---
+++ Assets/_Ellie/Scripts/Items/BuildingItem.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+namespace CarGame
+{
+    [CreateAssetMenu(menuName = "Car/Item/New Building")]
+    public class BuildingItem : ItemData
+    {
+        public Building prefab;
+    }
+}
---
+++ Assets/_Ellie/Scripts/Player Buildings/Building.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace CarGame
+{
+    public class Building : MonoBehaviour
+    {
+        [SerializeField] private GameObject visuals;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+    }
+}
